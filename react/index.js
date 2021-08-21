5e1c1b9d87d7006022db55c4421bd4b56e7d6f4d
lisp719 <368034+lisp719@users.noreply.github.com>
2021-08-21 04:49:16 +0000

    feat: Implement todo saving function

--- react/index.js
+++ react/index.js
@@ -1,16 +1,32 @@
-import { createElement, useState } from "//cdn.skypack.dev/react";
+import { createElement, useEffect, useState } from "//cdn.skypack.dev/react";
 import ReactDOM from "//cdn.skypack.dev/react-dom";
 import { html } from "//cdn.skypack.dev/htm/react";
 import { useForm } from "//cdn.skypack.dev/react-hook-form";
+import Dexie from "//cdn.skypack.dev/dexie";
+
+const db = new Dexie("react");
+
+db.version(1).stores({
+  todos: `++id, name`,
+});
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const { register, handleSubmit, setValue } = useForm();
 
   const onSubmit = ({ name }) => {
-    setTodos((todos) => todos.concat({ name }));
+    db.todos.add({ name });
+    fetchTodos();
     setValue("name", "");
   };
+
+  const fetchTodos = async () => {
+    setTodos(await db.todos.toArray());
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   return html`
     <div>
@@ -19,7 +35,11 @@ const App = () => {
         <button>add</button>
       </form>
       <ul>
-        ${todos.map((todo) => html`<li>${todo.name}</li>`)}
+        ${todos.map(
+          (todo) => html`<li>
+            <span>${todo.name}</span>
+          </li>`
+        )}
       </ul>
     </div>
   `;
